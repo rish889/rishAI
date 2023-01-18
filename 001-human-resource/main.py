3e41e9cff47013b51ef28b3f32dd04126a189927
Rishabh Garcha <rish889@gmail.com>
2023-01-18 15:37:04 +0530

    refactor

---
+++ 001-human-resource/main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+print("rishabh")
+employee_df = pd.read_csv('./Human_Resources.csv')
+print(employee_df.head(1).to_string())
